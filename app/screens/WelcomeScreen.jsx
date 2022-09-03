d7789ac6d4f8ff0d922a01c732e444106b61ae56
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-09-03 17:47:52 +0530

    Updated WelcomeScreen.jsx added a Blur Radius

--- app/screens/WelcomeScreen.jsx
+++ app/screens/WelcomeScreen.jsx
@@ -1,29 +1,49 @@
 import React from "react";
 import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+import AppButton from "../components/AppButton";
+import colors from "../config/colors";
 
 function WelcomeScreen(props) {
   return (
     <ImageBackground
+      blurRadius={3}
       style={styles.background}
       source={require("../assets/background.jpg")}
     >
       <View style={styles.logoContainer}>
         <Image style={styles.logo} source={require("../assets/logo-red.png")} />
-        <Text>Sell What You Don't Need</Text>
+        <Text style={styles.welcomeText}>Sell What You Don't Need</Text>
       </View>
-      <View style={styles.loginButton}></View>
-      <View style={styles.registerButton}></View>
+      <View style={styles.buttonContainer}>
+        <AppButton
+          title="Login"
+          onPress={() => {
+            console.log("Button Pressed");
+          }}
+          color={colors.primary}
+        />
+        <AppButton title="Register" color={colors.secondary} />
+      </View>
     </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  welcomeText: {
+    margin: 25,
+    fontSize: 15,
+    fontWeight: "700",
+    letterSpacing: 1,
+  },
   background: {
     flex: 1,
     justifyContent: "flex-end",
     alignItems: "center",
     // width: "100%",
     // height: "100%",
+  },
+  buttonContainer: {
+    width: "90%",
   },
   loginButton: {
     width: "100%",
