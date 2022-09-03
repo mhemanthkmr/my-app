8990e044ec7e9c14e5e1004d01e85b83b8e3325d
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-09-03 17:46:24 +0530

    Created a AppButton.jsx

---
+++ app/components/AppButton.jsx
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import colors from "../config/colors";
+export default function AppButton({ title, color }) {
+  return (
+    <TouchableOpacity style={[styles.button, { backgroundColor: color }]}>
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+    borderRadius: 25,
+    backgroundColor: colors.primary,
+    marginVertical: 10,
+  },
+  text: {
+    color: colors.white,
+    fontWeight: "bold",
+    fontSize: 18,
+    textTransform: "uppercase",
+  },
+});
