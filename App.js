5273b708ec0cb573f98ee28d93622fbae3c01f09
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-09-04 19:38:22 +0530

    Updated App.js

--- App.js
+++ App.js
@@ -1,9 +1,60 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  Touchable,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
-
+import Card from "./app/components/Card";
 export default function App() {
   return (
-      <WelcomeScreen />
+    <ScrollView>
+      <View style={{ backgroundColor: "#f8f4f4", padding: 20, paddingTop: 80 }}>
+        <Card
+          title="Red Jacket for Sale"
+          subTitle="$100"
+          image={require("./app/assets/chair.jpg")}
+        />
+        <Card
+          title="Red Jacket for Sale"
+          subTitle="$200"
+          image={require("./app/assets/background.jpg")}
+        />
+        <Card
+          title="Red Jacket for Sale"
+          subTitle="$100"
+          image={require("./app/assets/chair.jpg")}
+        />
+        <Card
+          title="Red Jacket for Sale"
+          subTitle="$290"
+          image={require("./app/assets/background.jpg")}
+        />
+        <Card
+          title="Red Jacket for Sale"
+          subTitle="$100"
+          image={require("./app/assets/chair.jpg")}
+        />
+        <Card
+          title="Red Jacket for Sale"
+          subTitle="$100"
+          image={require("./app/assets/background.jpg")}
+        />
+        <Card
+          title="Red Jacket for Sale"
+          subTitle="$100"
+          image={require("./app/assets/chair.jpg")}
+        />
+        <Card
+          title="Red Jacket for Sale"
+          subTitle="$100"
+          image={require("./app/assets/background.jpg")}
+        />
+      </View>
+    </ScrollView>
   );
 }
