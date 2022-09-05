84da878e604e314c48c57c558423f23d9014738f
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-09-05 19:46:45 +0530

    Added a ImageScreen in App.js

--- App.js
+++ App.js
@@ -12,7 +12,9 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import Card from "./app/components/Card";
 import ProductList from "./app/screens/ProductList";
 import ListingDetails from "./app/components/ListingDetails";
+import ImageScreen from "./app/screens/ImageScreen";
 export default function App() {
   // return <ListingDetails image={require("./app/assets/chair.jpg")} />;
-  return <ProductList />;
+  // return <ProductList />;
+  return <ImageScreen />;
 }
