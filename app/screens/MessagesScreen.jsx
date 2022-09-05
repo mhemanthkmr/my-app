90bbef60985fd6995e1316a46fbd2f83d1110582
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-09-05 22:29:00 +0530

    Created and MessageScreen.jsx and used Flat List and added a Varible called messages it contains json data

---
+++ app/screens/MessagesScreen.jsx
@@ -0,0 +1,48 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import ListItem from "../components/ListItem";
+import Screens from "../components/Screens";
+import colors from "../config/colors";
+import ListItemSeperator from "../components/ListItemSeperator";
+
+const messages = [
+  {
+    id: 1,
+    title: "T1",
+    description: "D1",
+    image: require("../assets/profile.jpg"),
+  },
+  {
+    id: 2,
+    title: "T2",
+    description: "D2",
+    image: require("../assets/couch.jpg"),
+  },
+  {
+    id: 3,
+    title: "T3",
+    description: "D3",
+    image: require("../assets/jacket.jpg"),
+  },
+];
+
+export default function MessagesScreen() {
+  return (
+    <Screens>
+      <FlatList
+        data={messages}
+        keyExtractor={(message) => message.id.toString()}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            subTitle={item.description}
+            image={item.image}
+          />
+        )}
+        ItemSeparatorComponent={() => <ListItemSeperator />}
+      />
+    </Screens>
+  );
+}
+
+const styles = StyleSheet.create({});
