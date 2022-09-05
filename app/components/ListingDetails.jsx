01334724165c3632cf54df3ea2029e488877d57f
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-09-05 19:14:25 +0530

    Added ListItem and created a userDetails in Style

--- app/components/ListingDetails.jsx
+++ app/components/ListingDetails.jsx
@@ -1,14 +1,21 @@
 import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 import colors from "../config/colors";
-
+import ListItem from "./ListItem";
 export default function ListingDetails({ image }) {
   return (
     <View>
       <Image style={styles.image} source={image} />
       <View style={styles.detailContainer}>
-        <Text style={styles.title}>Red jacket for sale</Text>
+        <Text style={styles.title}>Night Lamp for Sale</Text>
         <Text style={styles.price}>$100</Text>
+        <View style={styles.userContainer}>
+          <ListItem
+            title="HemanthKumar"
+            subTitle="5 Listing"
+            image={require("../assets/profile.jpg")}
+          />
+        </View>
       </View>
     </View>
   );
@@ -30,5 +37,8 @@ const styles = StyleSheet.create({
   price: {
     fontSize: 20,
     color: colors.secondary,
+  },
+  userContainer: {
+    marginVertical: 50,
   },
 });
