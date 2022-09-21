a7dce04bd7aa58d0382ddd600f81b08397f306fe
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-09-21 22:29:54 +0530

    Added a Navigation

--- App.js
+++ App.js
@@ -1,42 +1,99 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as React from "react";
+import { Button, View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-const Tweets = () => {
-  <Screen>
-    <Text>Hello Word</Text>
-  </Screen>;
-};
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MessagesScreen from "./app/screens/MessagesScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import LoginScreen from "./app/screens/LoginScreen";
+import RegisterScreen from "./app/screens/RegisterScreen";
+import ListingScreen from "./app/screens/ListingScreen";
 
-const TweetDetails = () => {
-  <Screen>
-    <Text>TweetDetails</Text>
-  </Screen>;
-};
-
-const TweetDetails2 = () => {
-  <Screen>
-    <Text>TweetDetails2</Text>
-  </Screen>;
-};
-
-const Stack = createNativeStackNavigator();
-
-const StackNavigator = () => {
-  <Stack.Navigator>
-    <Stack.Screen name="Tweets" component={Tweets} />
-    <Stack.Screen name="TweetDetail" component={TweetDetails} />
-    <Stack.Screen name="TweetDetails2" component={TweetDetails2} />
-  </Stack.Navigator>;
-};
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <NavigationContainer>
-      <StackNavigator />
-    </NavigationContainer>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("MessageScreen")}
+      />
+    </View>
   );
 }
 
-const styles = StyleSheet.create({});
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
 
-//719jme1d
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          options={{
+            headerStyle: {
+              backgroundColor: "tomato",
+            },
+            headerTintColor: "white",
+            headerShown: false,
+          }}
+          component={WelcomeScreen}
+        />
+        <Stack.Screen
+          name="MessageScreen"
+          options={{
+            headerStyle: {
+              backgroundColor: "tomato",
+            },
+            headerTintColor: "white",
+            headerShown: false,
+          }}
+          component={MessagesScreen}
+        />
+        <Stack.Screen
+          name="LoginScreen"
+          options={{
+            headerStyle: {
+              backgroundColor: "tomato",
+            },
+            headerTintColor: "white",
+            headerShown: false,
+          }}
+          component={LoginScreen}
+        />
+        <Stack.Screen
+          name="RegisterScreen"
+          options={{
+            headerStyle: {
+              backgroundColor: "tomato",
+            },
+            headerTintColor: "white",
+            headerShown: false,
+          }}
+          component={RegisterScreen}
+        />
+        <Stack.Screen
+          name="ListingScreen"
+          options={{
+            headerStyle: {
+              backgroundColor: "tomato",
+            },
+            headerTintColor: "white",
+            headerShown: false,
+          }}
+          component={ListingScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+    // <ListingScreen />
+  );
+}
+
+export default App;
+// ... other code from the previous section
